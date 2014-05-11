37d501e8917447004305e1de647443d5310085fe
Gabriel de Laboulaye <gabriel.lefebvre-de-laboulaye@student.ecp.fr>
2014-05-11 20:16:12 +0200

    Add some classes in model.
    Basic classes useful next: team, match, result.

--- paris/top14/models.py
+++ paris/top14/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class Team(models.Model):
+
+  name = models.CharField(max_length=30)
+
+
+class Match(models.Model):
+
+  homeTeam = models.CharField(max_length=30)
+  awayTeam = models.CharField(max_length=30)
+
+
+class Result(models.Model):
+
+  homeTeamPoints = models.PositiveSmallIntegerField(default=0)
+  awayTeamPoints = models.PositiveSmallIntegerField(default=0)
