c2e7fb1796b51db1a605add01ebf8d8aa1021aa6
Gabriel de Laboulaye <gabriel.lefebvre-de-laboulaye@student.ecp.fr>
2014-05-15 21:08:19 +0200

    Add a foreign key in Match with Result.

--- paris/top14/models.py
+++ paris/top14/models.py
@@ -6,13 +6,14 @@ class Team(models.Model):
   name = models.CharField(max_length=30)
 
 
-class Match(models.Model):
-
-  homeTeam = models.CharField(max_length=30)
-  awayTeam = models.CharField(max_length=30)
-
-
 class Result(models.Model):
 
   homeTeamPoints = models.IntegerField(default=0)
   awayTeamPoints = models.IntegerField(default=0)
+
+
+class Match(models.Model):
+
+  homeTeam = models.CharField(max_length=30)
+  awayTeam = models.CharField(max_length=30)
+  result = models.ForeignKey(Result)
