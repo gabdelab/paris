88c01f9fec58c5e4295026136daec8827fce382f
Gabriel de Laboulaye <gabriel.lefebvre-de-laboulaye@student.ecp.fr>
2014-05-27 23:58:29 +0200

    Add Team, Match and Result in admin view.

--- paris/top14/admin.py
+++ paris/top14/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from top14.models import Match, Team, Result
+
+
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(Result)
